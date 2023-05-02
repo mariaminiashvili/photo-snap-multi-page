15329b7f3bd9f240b3e4f838a7e969019cb65920
mariaminiashvili <mari.iniashvili@gmail.com>
2023-05-03 02:23:07 +0400

    change toggle button

--- pricing.js
+++ pricing.js
@@ -1,31 +1,38 @@
 
 
 const toggle = document.querySelector(".toggle");
+const toggleActive = document.querySelector(".toggle-active");
 const Monthly = document.querySelector(".Monthly");
 const Yearly = document.querySelector(".Yearly");
 const toggleButton = document.querySelector(".toggle-button");
-const active1 = document.querySelector(".toggle-button-active");
+const toggleButtonActive = document.querySelector(".toggle-button-active");
+const price = document.querySelector(".price");
+const priceOne = document.querySelector(".price-one");
+const priceTwo = document.querySelector(".price-two");
 
 
-let buttonActiv = false;
-
+let activButton = false;
 
 toggleButton.addEventListener("click" ,() =>{
- if(!buttonActiv){
-    buttonActiv=!buttonActiv;
-    textBar.style.display = "flex";
-    Yearly.classList,add.color = "red";
-    cross.style.display = "flex";
+ if(!activButton){
+    activButton=!activButton;
+    toggleButton.classList.remove ("toggle-button");
+    toggle.classList.add ("toggle-active");
+    toggleButton.classList.add ("toggle-button-active");
+    Monthly.style.color = "#DFDFDF";
+    Yearly.style.color = "#000000";
+    price.innerHTML = " $390.0";
+    priceOne.innerHTML = " $190.0";
+    priceTwo.innerHTML = " $990.0";
     }else{
-        hidden=!hidden;
-        textBar.style.display = "none";
-        bar.style.display = "flex";
-        cross.style.display = "none";
+        activButton=!activButton;
+        toggleButton.classList.add ("toggle-button");
+        toggle.classList.remove ("toggle-active");
+        toggleButton.classList.remove ("toggle-button-active");
+        Monthly.style.color = "#000000"
+        Yearly.style.color = "#DFDFDF"
+        price.innerHTML = "$39.00";
+        priceOne.innerHTML = " $19.0";
+        priceTwo.innerHTML = " $99.0";
  }
-})
-
-let togbut = false;
-
-toggleButton.addEventListener("click",()=>{
-  
 })
